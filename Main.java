621cdc03a6908f440f9b24872973f258e07244c8
Nicola <nginvernizzi1@gmail.com>
2022-10-13 21:11:11 +0200

    first

--- Main.java
+++ Main.java
@@ -1,5 +1,57 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
 
+    Scanner scanner = new Scanner(System.in);
+    Radio r1 = new Radio();
+
+    int choice;
+
+    do {
+      System.out.println("1. Accendi la radio");
+      System.out.println("2. Spegni la radio");
+      System.out.println("3. Imposta il volume");
+      System.out.println("4. Cambia il volume");
+      System.out.println("5. Seleziona il canale");
+      System.out.println("6. Cambia il canale");
+      System.out.println("7. Stato radio");
+      System.out.println("0. Esci");
+      System.out.print("\nScegli un'opzione: ");
+
+      choice = scanner.nextInt();
+
+      switch (choice) {
+        case 1:
+          r1.powerOn();
+          System.out.println("\nLa radio è accesa\n");
+          break;
+        case 2:
+          r1.powerOff();
+          System.out.println("\nLa radio è spenta\n");
+          break;
+        case 3:
+          System.out.print("A che livello vuoi impostare il volume? ");
+          r1.setVolume(scanner.nextInt());
+          break;
+        case 4:
+          System.out.print("Quanto vuoi modificare il volume? ");
+          r1.changeVolume(scanner.nextInt());
+          break;
+        case 5:
+          System.out.print("Che canale vuoi impostare? ");
+          r1.setChannel(scanner.nextFloat());
+          break;
+        case 6:
+          System.out.print("Quanto vuoi modificare il canale? ");
+          r1.changeChannel(scanner.nextFloat());
+          break;
+        case 7:
+          System.out.println("\n" + r1 + "\n");
+          break;
+      }
+    } while (choice != 0);
+
+    scanner.close();
   }
 }
--- Radio.java
+++ Radio.java
@@ -1,9 +1,100 @@
 class Radio {
   private boolean isOn;
   private int volume;
+  private int minVolume;
+  private int maxVolume;
   private float channel;
+  private float minChannel;
+  private float maxChannel;
 
   public Radio() {
-    this.isOn = false;
+    isOn = false;
+    volume = 50;
+    minVolume = 0;
+    maxVolume = 100;
+    minChannel = 87.5f;
+    channel = minChannel;
+    maxChannel = 108f;
+  }
+
+  public Radio(Radio r) {
+    this.isOn = r.isOn;
+    this.volume = r.volume;
+    this.minVolume = r.minVolume;
+    this.maxVolume = r.maxVolume;
+    this.channel = r.channel;
+    this.minChannel = r.minChannel;
+    this.maxChannel = r.maxChannel;
+  }
+
+  public Radio copyMoto() {
+    return new Radio(this);
+  }
+
+  public String getStatus() {
+    return isOn ? "La radio è accesa" : "La radio è spenta";
+  }
+
+  public String getVolume() {
+    return "Il volume è al " + volume + "%";
+  }
+
+  public String getChannel() {
+    return "Il canale è " + channel + "MHz";
+  }
+
+  public void powerOn() {
+    isOn = true;
+  }
+
+  public void powerOff() {
+    isOn = false;
+  }
+
+  public String setVolume(int volume) {
+    if (volume > minVolume && volume < maxVolume) {
+      this.volume = volume;
+      return "Il volume è al " + this.volume;
+    } else {
+      return "Imposta un volume tra 0 e 100%";
+    }
+  }
+
+  public String changeVolume(int volume) {
+    int delta = this.volume + volume;
+    if (delta > minVolume && delta < maxVolume) {
+      this.volume = delta;
+      return "Il volume è al " + this.volume;
+    } else {
+      return "Imposta un volume tra 0 e 100%";
+    }
+  }
+
+  public String setChannel(float channel) {
+    if (channel > minChannel && channel < maxChannel) {
+      this.channel = channel;
+      return "Il canale è " + this.channel;
+    } else {
+      return "Seleziona un canale tra 87.5MHz e 108MHz";
+    }
+  }
+
+  public String changeChannel(float channel) {
+    float delta = this.channel + channel;
+    if (delta > minChannel && delta < maxChannel) {
+      this.channel = delta;
+      return "Il canale è " + this.channel;
+    } else {
+      return "Seleziona un canale tra 87.5MHz e 108MHz";
+    }
+  }
+
+  public String toString() {
+    return "Stato: " + (isOn ? "accesa" : "spenta") + "\nVolume: " + volume + "%" + "\nCanale: "
+        + channel + "MHz";
+  }
+
+  public boolean equals(Radio r) {
+    return this.volume == r.volume && this.channel == r.channel;
   }
 }
